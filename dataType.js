14afab3742b901a5ef266326f50aeb05aa343ded
sadiinfo2 <sadi.info2@gmail.com>
2024-04-23 16:51:31 +0600

    dataType Example

---
+++ dataType.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 10.5;
+let c = "hello World";
+let d = 'Hello ';
+let e = true;
+let u
+let n = null;
+
+console.log("a = " + a +" " + "is" + " " +(typeof a));
+console.log("b = " + b +" " + "is" + " " +(typeof b));
+console.log("c = " + c +" " + "is" + " " +(typeof c));
+console.log("d = " + d +" " + "is" + " " +(typeof d));
+console.log("u = " + u +" " + "is" + " " +(typeof u));
+console.log("n = " + n +" " + "is" + " " +(typeof n));
